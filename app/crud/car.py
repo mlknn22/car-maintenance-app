07d26c723cc16c362c14f929e319ed32421550be
Narek <melkonyannarek@bk.ru>
2026-03-14 12:09:19 +0300

    Add remaining CRUD for cars

--- app/crud/car.py
+++ app/crud/car.py
@@ -18,7 +18,37 @@ def create_car(db: Session, car: CarCreate):
 
     return db_car
 
-
 def get_cars(db: Session):
 
     return db.query(Car).all()
+
+def get_car_by_id(db: Session, car_id: int):
+    db.query(Car).filter(Car.id == car_id).first()
+
+def update_car(db: Session, car_id: int, car_data: CarCreate):
+    car =  db.query(Car).filter(Car.id == car_id).first()
+
+    if not car:
+        return None
+    
+    car.brand = car_data.brand
+    car.model = car_data.model
+    car.mileage = car_data.mileage
+    car.year = car_data.year
+
+    db.commit()
+    db.refresh(car)
+
+    return car
+
+def delete_car(db: Session, car_id: int):
+    car =  db.query(Car).filter(Car.id == car_id).first()
+
+    if not car:
+        return None
+    
+    db.delete(car)
+    db.commit()
+
+    return car
+
