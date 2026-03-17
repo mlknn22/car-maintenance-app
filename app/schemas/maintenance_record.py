b90e089c7ddae58a4d862faed74e98c1aaf6db72
Narek Melkonyan <melkonyannarek@bk.ru>
2026-03-17 22:38:35 +0300

    feat: Add MaintenanceRecord schemas

---
+++ app/schemas/maintenance_record.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field
+from datetime import date, datetime
+
+class MaintenanceRecordBase(BaseModel):
+    car_id: int = Field(..., gt=0)
+    service_date: date
+    work_type: str = Field(..., min_lentgh = 2)
+    cost: float = Field(..., ge=0, description="Стоимость работ в рублях")
+    mileage_at_service: int = Field(..., ge=0, description="Пробег автомобиля на момент проведения ТО в километрах")
+    notes: str | None = None
+
+
+class MaintenanceRecordCreate(MaintenanceRecordBase):
+    pass
+
+
+class MaintenanceRecordUpdate(BaseModel):
+    service_date: date | None =  None
+    work_type: str | None = Field(None, min_length = 2)
+    cost: float | None = Field(None, ge=0)
+    mileage_at_service: int | None = Field(None, ge=0)
+    notes: str | None = None
+
+
+class MaintenanceRecordResponse(MaintenanceRecordBase):
+    id: int
+    created_at: datetime
+
+
+    model_config = {"from_attributes": True}
+
+
