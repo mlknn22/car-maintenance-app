d3c553691b194f623415859be14ec60172b474b4
Narek Melkonyan <melkonyannarek@bk.ru>
2026-03-17 19:30:32 +0300

    feat: Add model MaintenanceRecord model

---
+++ app/models/maintenance_record.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, Integer, Float, Date, ForeignKey
+from sqlalchemy import func
+from sqlalchemy import DateTime
+from app.db.database import Base
+
+
+class MaintenanceRecord(Base):
+    __tablename__ = "maintenance_records"
+
+
+    id = Column(Integer, primary_key=True, index=True)
+    car_id = Column(Integer, ForeignKey("cars.id"), nullable=False)
+    service_date = Column(Date, nullable=False)
+    work_type = Column(String, nullable=False)
+    cost = Column(Float, nullable=False)
+    mileage_at_service = Column(Integer, nullable=False)
+    notes = Column(String, nullable=True)
+    created_at = Column(DateTime, server_default=func.now()) #сервер сам ставит время
