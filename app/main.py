199c4e8eb1a413201de7d58d3c626c6ff292f3cd
Narek Melkonyan <melkonyannarek@bk.ru>
2026-03-19 20:06:30 +0300

    feat(main): connect telemetry_logs router and model

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
-from app.routes import (
-    cars, devices, maintenance_records
-)
+from app.routes import cars, devices, maintenance_records, telemetry_logs
 from app.db.database import engine, Base
-from app.models import car, device, maintenance_record
+from app.models import car, device, maintenance_record, telemetry_log
 
-app = FastAPI(
-    title="Car Maintenance API"
-)
+app = FastAPI(title="Car Maintenance API")
 
 @app.get("/")
 async def root():
@@ -16,5 +12,7 @@ async def root():
 app.include_router(cars.router)
 app.include_router(devices.router)
 app.include_router(maintenance_records.router)
+app.include_router(telemetry_logs.router)
+
 
 Base.metadata.create_all(bind=engine)
